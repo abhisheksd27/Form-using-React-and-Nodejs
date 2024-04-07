300691e21825572fd270358a41d723caa2d42569
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-04-07 19:54:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+
+app.use(cors());
+
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+  const { name, email, phone } = req.body;
+  console.log('Form data:', { name, email, phone });
+  res.status(200).json({ message: 'Form submitted successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
